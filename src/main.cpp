6aedf994d8b053089cbb2335998fccf1901cb215
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2020-04-26 19:50:14 +0200

    get ready for frsh start with new mpUtils version

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,6 @@
 #include <mpUtils/mpUtils.h>
 #include <mpUtils/mpGraphics.h>
 
-#include "engine/SpriteRenderer.h"
-#include "engine/Camera2D.h"
-#include "gameLogic/GameObject.h"
-
 void addGeneralKeys()
 {
     using namespace mpu::gph;
@@ -38,66 +34,13 @@ int main()
     // setup keybindings
     addGeneralKeys();
 
-    // have a vao active because we have to have one
-    VertexArray vao;
-    vao.bind();
-
-    // glsettings
+    // background color
     glClearColor(0.2,0.3,0.5,1.0);
-    glDisable(GL_DEPTH_TEST);
-
-    SpriteRenderer::setProjection(glm::mat4(1));
-
-    Sampler sampler;
-    sampler.set(GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
-    sampler.set(GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    sampler.bind(GL_TEXTURE0);
-
-    // setup camera
-    Camera2D camera(mainWnd.getFramebufferSize().x / mainWnd.getFramebufferSize().y, 0.1, 0.4);
-    mainWnd.addSizeCallback([&camera](int x,int y)
-    {
-        camera.setAspect(1.0f * x / y);
-        glViewport(0,0,x,y);
-    });
-
-    // varibles
-    glm::vec2 size{0.1,0.28};
-    glm::vec2 pos{0,0};
-    float rot =0;
-    bool showCameraWindow = true;
-
-    // setup level
-    std::vector<std::unique_ptr<GameObject>> objects;
-
-    objects.emplace_back(std::make_unique<GameObject>(PROJECT_RESOURCE_PATH "Background.png",glm::vec2(80.0f,80.0f)));
-    objects.emplace_back(std::make_unique<GameObject>(PROJECT_RESOURCE_PATH "car_test01.png",glm::vec2(1.0f,2.8f)));
-
-    camera.setMoveBoundary({-40,-40,40,40});
-    camera.setZoomBoundry({0,100});
 
     // start main loop
     while(mainWnd.frameEnd(), Input::update(), mainWnd.frameBegin())
     {
-        camera.update();
-        SpriteRenderer::setProjection(camera.getProj());
 
-        for(auto &object : objects)
-        {
-            object->update();
-        }
-
-        for(auto &object : objects)
-        {
-            object->draw();
-        }
-
-        // im gui debug window
-        {
-        }
-
-        if(showCameraWindow)
-            camera.drawGui();
     }
 
 }
--- todo.md
+++ todo.md
@@ -1,10 +1,2 @@
 # todo
 
-## camera
-- add better smoothing for 2d camera
-- add mouse camera movement
-- adjust movemnt speed with zoom
-
-## game objects
-- add debug menue
-- add default positions
